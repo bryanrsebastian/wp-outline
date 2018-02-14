1fee1d4c0c515b47eaf5b594109faea6f038262e
Bryan Sebastian <bryanrsebastian@gmail.com>
2018-02-14 13:21:00 +0800

    Update font awesome

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -10,12 +10,12 @@ if ( ! function_exists( 'enqueue_style_script' ) ) {
          */ 
         
         /* Third Party Styles */
-        wp_enqueue_style('font_awesome', THEME_URL .'/vendors/font-awesome/css/font-awesome.min.css');
-        wp_enqueue_style('bootstrap_css', THEME_URL .'/vendors/bootstrap/dist/css/bootstrap.min.css');
-        wp_enqueue_style('sweetalert2_css', THEME_URL .'/vendors/sweetalert2/dist/sweetalert2.min.css');
+        wp_enqueue_style('fontawesome-free-5.0.6', THEME_URL .'/vendors/fontawesome-free-5.0.6/web-fonts-with-css/css/fontawesome-all.min.css');
+        wp_enqueue_style('bootstrap4-beta', THEME_URL .'/vendors/bootstrap4-beta/dist/css/bootstrap.min.css');
+        wp_enqueue_style('sweetalert2', THEME_URL .'/vendors/sweetalert2/dist/sweetalert2.min.css');
         
         /* Custom Styles */
-        wp_enqueue_style('general_css', THEME_URL .'/assets/style/css/general.css');
+        wp_enqueue_style('general', THEME_URL .'/assets/style/css/general.css');
         
         /* 
          * Register/Hook Scripts
@@ -25,17 +25,17 @@ if ( ! function_exists( 'enqueue_style_script' ) ) {
         wp_register_script( 'nicescroll', THEME_URL  .'/vendors/nicescroll/jquery.nicescroll.min.js', array('jquery'), NULL, true );
         wp_enqueue_script( 'nicescroll' );
         
-        wp_register_script( 'popper_js', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js', array('jquery'), NULL, true );
-        wp_enqueue_script( 'popper_js' );
+        wp_register_script( 'popper', '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js', array('jquery'), NULL, true );
+        wp_enqueue_script( 'popper' );
         
-        wp_register_script( 'bootstrap_js', THEME_URL  .'/vendors/bootstrap/dist/js/bootstrap.min.js', array('jquery'), NULL, true );
-        wp_enqueue_script( 'bootstrap_js' );
+        wp_register_script( 'bootstrap4-beta', THEME_URL  .'/vendors/bootstrap4-beta/dist/js/bootstrap.min.js', array('jquery'), NULL, true );
+        wp_enqueue_script( 'bootstrap4-beta' );
 
-        wp_register_script( 'sweetalert2_js', THEME_URL  .'/vendors/sweetalert2/dist/sweetalert2.min.js', array('jquery'), NULL, true );
-        wp_enqueue_script( 'sweetalert2_js' );
+        wp_register_script( 'sweetalert2', THEME_URL  .'/vendors/sweetalert2/dist/sweetalert2.min.js', array('jquery'), NULL, true );
+        wp_enqueue_script( 'sweetalert2' );
 
-        wp_register_script( 'sweetalert2_support_js', '//cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js', array('jquery'), NULL, true );
-        wp_enqueue_script( 'sweetalert2_support_js' );
+        wp_register_script( 'sweetalert2_support', '//cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js', array('jquery'), NULL, true );
+        wp_enqueue_script( 'sweetalert2_support' );
 
         /* AJAX Script */
         // wp_register_script( 'function-name', THEME_URL . '/inc/ajax/function-name/function-name.js', array( 'jquery' ), NULL, true );
@@ -62,31 +62,31 @@ if ( ! function_exists( 'enqueue_style_script' ) ) {
             /**
              * Enqueue your style/script only @ IE
              */
-            wp_enqueue_style('general_css', THEME_URL .'/assets/style/css/ie.css');
+            wp_enqueue_style('ie', THEME_URL .'/assets/style/css/ie.css');
         } 
         elseif( preg_match( '/firefox/i', $u_agent ) ) {
             /**
              * Enqueue your style/script only @ Mozilla Firefox
              */
-            wp_enqueue_style('general_css', THEME_URL .'/assets/style/css/firefox.css');
+            wp_enqueue_style('firefox', THEME_URL .'/assets/style/css/firefox.css');
         } 
         elseif( preg_match( '/mac/i', $u_agent ) ) {
             /**
              * Enqueue your style/script only @ Safari
              */
-            wp_enqueue_style('general_css', THEME_URL .'/assets/style/css/safari.css');
+            wp_enqueue_style('safari', THEME_URL .'/assets/style/css/safari.css');
         } 
         elseif( preg_match( '/chrome/i', $u_agent ) ) {
             /**
              * Enqueue your style/script only @ Google Chrome
              */
-            wp_enqueue_style('general_css', THEME_URL .'/assets/style/css/chrome.css');
+            wp_enqueue_style('chrome', THEME_URL .'/assets/style/css/chrome.css');
         } 
         elseif( preg_match( '/Opera/i',$u_agent ) || preg_match( '/OPR/i',$u_agent ) ) {
             /**
              * Enqueue your style/script only @ Opera
              */
-            wp_enqueue_style('general_css', THEME_URL .'/assets/style/css/opera.css');
+            wp_enqueue_style('opera', THEME_URL .'/assets/style/css/opera.css');
         }
     }
 }
